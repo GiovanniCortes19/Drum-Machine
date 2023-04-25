c289b0bdf79837a752213278e906f253b35b9d42
GiovanniCortes <giocortes19@gmail.com>
2023-04-25 16:17:56 -0400

    made sound play when drum-pad is clicked

--- src/components/SoundKey.jsx
+++ src/components/SoundKey.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const SoundKey = (props) => {
+
+    function playSound () {
+        document.getElementById(props.keyID).play()
+    }
+
   return (
-    <div className='drum-pad' id={props.name}>
+    <div className='drum-pad' id={props.name} onClick={playSound}>
         <h3>{props.keyID}</h3>
         <audio src={props.audio} className='clip' id={props.keyID}></audio>
     </div>
