6374ed82463d4a29cd1b0921325c00c6306a07c5
GiovanniCortes <giocortes19@gmail.com>
2023-04-25 00:53:04 -0400

    added style and ids

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ function App() {
 
 
   return (
-    <>
+    <div id='drum-machine'>
       <h1>Drum Machine</h1>
-      <div id='drumBox-container'>
+      <div id='display' className='drumBox-container'>
         <div>SoundBoard</div>
         <div>ControllerStation</div>
       </div>
-    </>
+    </div>
   )
 }
 
