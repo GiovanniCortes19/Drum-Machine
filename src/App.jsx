8db63eaf1caf7ce2253f82cf80bc22279962e15b
GiovanniCortes <giocortes19@gmail.com>
2023-04-24 21:10:00 -0400

    initialized files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Drum Machine</h1>
+      <div id='drumBox-container'>
+        <div>SoundBoard</div>
+        <div>ControllerStation</div>
+      </div>
+    </>
+  )
+}
+
+export default App
