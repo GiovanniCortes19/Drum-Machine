1887d6e7fe0aab57eb44254f2b083917b0381f9b
GiovanniCortes <giocortes19@gmail.com>
2023-04-25 17:25:25 -0400

    created initial syling of power button

--- src/components/ControllerStation.jsx
+++ src/components/ControllerStation.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const ControllerStation = () => {
   return (
     <div className='controllerStation'>
-        <div className="powerBtn">power flick</div>
+        <div className="power">
+            <p>POWER</p>
+            <div className="btn-power">
+                <div className="flicker"></div>
+            </div>
+        </div>
         <h2 id='display'>Testing</h2>
         <div className='volumeControl'>volume</div>
     </div>
