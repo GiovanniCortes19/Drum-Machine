2d60700138251473229ccf2a41ecdc273aadb802
GiovanniCortes <giocortes19@gmail.com>
2023-04-25 16:23:34 -0400

    added all the sounds to the sounds file

--- src/components/sounds.js
+++ src/components/sounds.js
@@ -10,49 +10,49 @@ const sounds = [
         id: 2,
         name: 'example',
         keyID: 'W',
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
     },
     {
         id: 3,
         name: 'example',
         keyID: 'E',
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
     },
     {
         id: 4,
         name: 'example',
         keyID: 'A',
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
     },
     {
         id: 5,
         name: 'example',
         keyID: 'S',
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
     },
     {
         id: 6,
         name: 'example',
         keyID: 'D',
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
     },
     {
         id: 7,
         name: 'example',
         keyID: 'Z',
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
     },
     {
         id: 8,
         name: 'example',
         keyID: 'X',
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
     },
     {
         id: 9,
         name: 'example',
         keyID: 'C',
-        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+        audio: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
     },
 ]
 
