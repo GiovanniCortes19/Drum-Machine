ba5003cfdc5952508d09a2a2804975d83d0b4210
GiovanniCortes <giocortes19@gmail.com>
2023-04-27 16:43:22 -0400

    cleaned up commented-out code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
 import SoundBoard from './components/SoundBoard'
-// import ControllerStation from './components/ControllerStation'
-
 
 function App() {
 
--- src/components/SoundKey.jsx
+++ src/components/SoundKey.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useCallback } from 'react'
 
 const SoundKey = (props) => {
 
